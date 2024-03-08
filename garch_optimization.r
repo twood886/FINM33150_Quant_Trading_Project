08c91aa77adfcd64802fe6dd943004b8d9a51935
JoshuaWeekes <joshweekes@uchicago.edu>
2024-03-08 17:07:16 -0600

    fixed syntax of garch stuff

--- garch_optimization.r
+++ garch_optimization.r
@@ -1,7 +1,7 @@
-if (!require('rmgarch')) install.packages('rmgarch')
-if (!require('rugarch')) install.packages('rugarch')
-if (!require('fGarch')) install.packages('fGarch')
-if (!require('quadprog')) install.packages('quadprog')
+if (!require("rmgarch")) install.packages("rmgarch")
+if (!require("rugarch")) install.packages("rugarch")
+if (!require("fGarch")) install.packages("fGarch")
+if (!require("quadprog")) install.packages("quadprog")
 
 library(rmgarch)
 library(rugarch)
@@ -10,25 +10,31 @@ library(quadprog)
 
 # assumes data is structured as a dataframe with columns for each asset
 
-garchSpec <- ugarchspec(variance.model = list(model = 'sGarch'), 
-                        mean.model = list(armaOrder = c(1, 1)), 
-                        distribution.model = 'norm')
+garch_spec <- ugarchspec(variance.model = list(model = "sGarch"),
+                         mean.model = list(armaOrder = c(1, 1)),
+                         distribution.model = "norm")
 
-dccSpec <- dccspec(uspec = multispec(replicate(4, garchSpec) ), dccOrder = c(1, 1), distribution = 'mvnorm')
+dcc_spec <- dccspec(uspec = multispec(replicate(4, garch_spec)),
+                    dccOrder = c(1, 1),
+                    distribution = "mvnorm")
 
-dccFit <- dccfit(dccSpec, data = replication_and_bdc_data)
+dcc_fit <- dccfit(dccSpec, data = replication_and_bdc_data)
 
-dccCov <- rcov(dccFit)
+dcc_cov <- rcov(dcc_fit)
 
 # Set target mean to 0 to minimize variance
-n <- ncol(dccCov)
+n <- ncol(dcc_cov)
 mu <- rep(0, n)
 
 # Set up the constraints
-Amat <- matrix(1, n, 1)
+amat <- matrix(1, n, 1)
 bvec <- 1
 
-optimization_result <- solve.QP(Dmat=dccCov, dvec=mu, Amat=Amat, bvec=bvec, meq=1)
+optimization_result <- solve.QP(Dmat = dcc_cov,
+                                dvec = mu,
+                                Amat = amat,
+                                bvec = bvec,
+                                meq = 1)
 
 hedged_portfolio_weights <- optimization_result$solution
 
