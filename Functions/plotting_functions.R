012669e8b03212d7bc8f5775df1b0b851a718c7c
twood886 <twood886@gmail.com>
2024-03-08 20:16:18 -0500

    more updates

--- Functions/plotting_functions.R
+++ Functions/plotting_functions.R
@@ -2,11 +2,12 @@ library(tidyverse)
 library(lubridate)
 library(scales)
 library(forecast)
-library(cowplot)
-library(ggpubr)
 # Plot Functions ----------------------------------------------------------
 
-makeReturnPlots <- function(name, daily_data, datecol, pricecol=NULL, returncol = NULL){
+
+
+# Single Security ---------------------------------------------------------
+makeReturnPlots <- function(name, data, datecol, pricecol=NULL, returncol = NULL){
 
   if(is.null(pricecol) & is.null(returncol)){
     stop("Must specify price or return col")
@@ -56,15 +57,28 @@ makeReturnPlots <- function(name, daily_data, datecol, pricecol=NULL, returncol 
     "Weekly" = return_data_weekly,
     "Monthly" = return_data_monthly)
 
-  index_plots <- map2(returns_data, names(returns_data), ~plot_index(name, .y, .x))
+  summary <- purrr::map2(returns_data, names(returns_data), ~return_summary(.y, .x))
+  index_plots <- purrr::map2(returns_data, names(returns_data), ~plot_index(name, .y, .x))
   hist_plots <- map2(returns_data, names(returns_data), ~plot_return_hist(name, .y, .x))
   hist_plot_all <- plot_return_hist(name, names(returns_data), returns_data)
+
   qq_plots <- map2(returns_data, names(returns_data), ~plot_return_qq(name, .y, .x))
   qq_plot_all <- plot_return_qq(name, names(returns_data), returns_data)
   acfpcf_plots <- map2(returns_data, names(returns_data), ~plot_return_acfpcf(name, .y, .x))
   return_plots <- map2(returns_data, names(returns_data), ~plot_return_ts(name, .y, .x))
   return_plot_all <- plot_return_ts(name, names(returns_data), returns_data)
 
+  return(list(
+    "data" = returns_data,
+    "summary" = summary,
+   "index_plots" = index_plots,
+   "hist_plots" = hist_plots,
+   "hist_plot_all" = hist_plot_all,
+   "qq_plots" = qq_plots,
+   "qq_plot_all" = qq_plot_all,
+   "acfpcf_plots" = acfpcf_plots,
+   "return_plots" = return_plots,
+   "return_plot_all" = return_plot_all))
 
   # plot_grid(
   #   hist_plots[["Daily"]]  + labs(title = NULL, subtitle = "Distribution of Returns"),
@@ -75,7 +89,6 @@ makeReturnPlots <- function(name, daily_data, datecol, pricecol=NULL, returncol 
 
 
 }
-
 plot_index <- function(name, frq, data){
   title = paste(name, " - Growth of $1000")
   start.date <- min(data$start, na.rm = T)
@@ -83,9 +96,9 @@ plot_index <- function(name, frq, data){
 
   subtitle <- paste(
     "Data from",
-    as.character(start.date, format = "%b %d, %Y"),
+    format(start.date, format = "%b %d, %Y"),
     "to",
-    as.character(end.date, format = "%b %d, %Y"),
+    format(end.date, format = "%b %d, %Y"),
     paste0("(", frq, ")"))
 
   p <- data %>%
@@ -103,7 +116,6 @@ plot_index <- function(name, frq, data){
     theme_classic()
   return(p)
 }
-
 plot_return_hist <- function(name, frq, data){
 
   if(!is.data.frame(data)){
@@ -117,9 +129,9 @@ plot_return_hist <- function(name, frq, data){
     end.date <- max(data$date)
     subtitle <- paste(
       "Data from",
-      as.character(start.date, format = "%b %d, %Y"),
+      format(start.date, format = "%b %d, %Y"),
       "to",
-      as.character(end.date, format = "%b %d, %Y"))
+      format(end.date, format = "%b %d, %Y"))
 
     p <- data  %>%
       tidyr::drop_na(`return`) %>%
@@ -143,9 +155,9 @@ plot_return_hist <- function(name, frq, data){
     end.date <- max(data$date)
     subtitle <- paste(
       "Data from",
-      as.character(start.date, format = "%b %d, %Y"),
+      format(start.date, format = "%b %d, %Y"),
       "to",
-      as.character(end.date, format = "%b %d, %Y"),
+      format(end.date, format = "%b %d, %Y"),
       paste0("(", frq, ")"))
 
     p <- data %>%
@@ -163,7 +175,6 @@ plot_return_hist <- function(name, frq, data){
   }
   return(p)
 }
-
 plot_return_qq <- function(name, frq, data){
 
   if(!is.data.frame(data)){
@@ -177,9 +188,9 @@ plot_return_qq <- function(name, frq, data){
     end.date <- max(data$date)
     subtitle <- paste(
       "Data from",
-      as.character(start.date, format = "%b %d, %Y"),
+      format(start.date, format = "%b %d, %Y"),
       "to",
-      as.character(end.date, format = "%b %d, %Y"))
+      format(end.date, format = "%b %d, %Y"))
 
     p <- data  %>%
       tidyr::drop_na(`return`) %>%
@@ -204,9 +215,9 @@ plot_return_qq <- function(name, frq, data){
     end.date <- max(data$date)
     subtitle <- paste(
       "Data from",
-      as.character(start.date, format = "%b %d, %Y"),
+      format(start.date, format = "%b %d, %Y"),
       "to",
-      as.character(end.date, format = "%b %d, %Y"),
+      format(end.date, format = "%b %d, %Y"),
       paste0("(", frq, ")"))
 
     p <- data %>%
@@ -222,10 +233,10 @@ plot_return_qq <- function(name, frq, data){
         subtitle = subtitle) +
       theme_classic()
 
-    return(p)
-  }
-}
 
+  }
+  return(p)
+}
 plot_return_acfpcf <- function(name, frq, data){
 
   lag.max <- case_when(
@@ -239,9 +250,9 @@ plot_return_acfpcf <- function(name, frq, data){
   subtitle <- paste(
     lag.max, "Lags",
     "Data from",
-    as.character(start.date, format = "%b %d, %Y"),
+    format(start.date, format = "%b %d, %Y"),
     "to",
-    as.character(end.date, format = "%b %d, %Y"),
+    format(end.date, format = "%b %d, %Y"),
     paste0("(", frq, ")"))
 
   x <- as.data.frame(drop_na(data,return)$return)
@@ -262,8 +273,8 @@ plot_return_acfpcf <- function(name, frq, data){
     facet_wrap(vars(`id`), scales = "free") +
     geom_segment(aes(xend=lag,yend=0), color = "#112446") +
     theme_classic() +
-    geom_hline(yintercept=ci.line.pcf, color="red", linetype="dashed") +
-    geom_hline(yintercept=-ci.line.pcf, color="red", linetype="dashed") +
+    geom_hline(yintercept=ci.line, color="red", linetype="dashed") +
+    geom_hline(yintercept=-ci.line, color="red", linetype="dashed") +
     labs(
       x = "Auto and Partial Auto Correlation",
       y = "Lags",
@@ -272,7 +283,6 @@ plot_return_acfpcf <- function(name, frq, data){
 
   return(p)
 }
-
 plot_return_ts <- function(name, frq, data){
   if(!is.data.frame(data)){
 
@@ -285,9 +295,9 @@ plot_return_ts <- function(name, frq, data){
     end.date <- max(data$date)
     subtitle <- paste(
       "Data from",
-      as.character(start.date, format = "%b %d, %Y"),
+      format(start.date, format = "%b %d, %Y"),
       "to",
-      as.character(end.date, format = "%b %d, %Y"))
+      format(end.date, format = "%b %d, %Y"))
 
     p <- data  %>%
       tidyr::drop_na(`return`) %>%
@@ -311,9 +321,9 @@ plot_return_ts <- function(name, frq, data){
     end.date <- max(data$date)
     subtitle <- paste(
       "Data from",
-      as.character(start.date, format = "%b %d, %Y"),
+      format(start.date, format = "%b %d, %Y"),
       "to",
-      as.character(end.date, format = "%b %d, %Y"),
+      format(end.date, format = "%b %d, %Y"),
       paste0("(", frq, ")"))
 
     p <- data %>%
@@ -330,5 +340,107 @@ plot_return_ts <- function(name, frq, data){
         title =  title,
         subtitle = subtitle)
   }
+  return(p)
+}
+return_summary <- function(frq, data){
+  mult <- case_when(
+    frq == "Daily" ~ 252,
+    frq == "Weekly" ~ 52,
+    frq == "Monthly" ~ 12)
+  data <- data %>%
+    filter(!is.na(`return`))
+
+  total_return <- prod(1+data$return) -1
+  annmean <- mean(data$return) * mult
+  annvol <- sd(data$return) * sqrt(mult)
+  downside_vol <- sd(ifelse(data$return <0 , data$return, 0)) * sqrt(mult)
+  skew <- DescTools::Skew(data$return)
+  kurtosis <- DescTools::Kurt(data$return)
+
+  Sharpe <- annmean / annvol
+  Sortino <- annmean / downside_vol
+
+  previous_peaks <- cummax(data$index)
+  drawdowns <- (data$index - previous_peaks) / previous_peaks
+  max_drawdown <- min(drawdowns)
+
+  sum <-c(
+    paste("Total Return:", paste0(formatC(total_return * 100, digits = 3),"%"), sep = "\t\t"),
+    paste("Ann. Mean Returns:", paste0(formatC(annmean * 100, digits = 3),"%"), sep = "\t"),
+    paste("Ann. Return StdDev:", paste0(formatC(annvol * 100, digits = 3),"%"), sep = "\t"),
+    paste("Sharpe Ratio (no rf):", formatC(Sharpe, digits = 3), sep = "\t"),
+    paste("Sortino Ratio (no rf):", formatC(Sortino, digits = 3), sep = "\t"),
+    paste("Skewness:", formatC(skew, digits = 2), sep = "\t\t"),
+    paste("Excess Kurtosis", formatC(kurtosis, digits = 3), sep = "\t\t"),
+    paste("Max Drawdown:", paste0(formatC(max_drawdown * 100, digits = 3),"%"), sep = "\t\t"))
+
+  return(sum)
+}
+
+
+
+
+
+plot_2_qq <- function(names, data, frq){
+  name1 <- names[[1]]
+  name2 <- names[[2]]
+  data1 <- data[[1]] %>% drop_na(`return`)
+  data2 <- data[[2]] %>% drop_na(`return`)
+
+  gg_qq_empirical <- function(a, b, quantiles = seq(0, 1, 0.01))
+  {
+    a_lab <- deparse(substitute(a))
+    if(missing(b)) {
+      b <- rnorm(length(a), mean(a), sd(a))
+      b_lab <- "normal distribution"
+    }
+    else b_lab <- deparse(substitute(b))
+
+    ggplot(mapping = aes(x = quantile(a, quantiles),
+                         y = quantile(b, quantiles))) +
+      geom_point() +
+      geom_abline(aes(slope = 1, intercept = 0), linetype = 2) +
+      labs(x = paste(deparse(substitute(a)), "quantiles"),
+           y = paste(deparse(substitute(b)), "quantiles"),
+           title = paste("Empirical qq plot of", a_lab, "against", b_lab))
+  }
+
+  title = paste("QQ Plot of", name1, "vs", name2)
+  subtitle = paste(frq, "Data")
+
+  p <- gg_qq_empirical(data2$return, data1$return) +
+    theme_classic() +
+    labs(
+      x = paste(name2, "Quantiles"),
+      y = paste(name1, "Quantiles"),
+      title =  title,
+      subtitle = subtitle)
+  return(p)
+}
+
+plot_roll_corr <- function(names, data, frq, n){
+  name1 <- names[[1]]
+  name2 <- names[[2]]
+  data1 <- data[[1]] %>% drop_na(`return`) %>% dplyr::select(`date`, `return`)
+  data2 <- data[[2]] %>% drop_na(`return`) %>% dplyr::select(`date`, `return`)
+
+  data3 <- inner_join(data1, data2, by = "date") %>%
+    tidyr::nest(data = c(everything(),-`date`), .by = `date`) %>%
+    dplyr::mutate(`rolldata` = purrr::accumulate(`data`, roll_bind, n)) %>%
+    dplyr::slice(-(1:13)) %>%
+    dplyr::mutate(`cor` = map(`rolldata`, \(x) cor(x$return.x, x$return.y))) %>%
+    dplyr::select(`date`, `cor`) %>%
+    tidyr::unnest(everything())
+
+  title <- paste(name1,"-",name2,"Rolling",n,frq,"Correlation")
+  p <- data3 %>%
+    ggplot() +
+    aes(x = date, y = cor) +
+    geom_line(colour = "#112446") +
+    theme_classic()+
+    labs(
+      x = "Date",
+      y = "Correlation",
+      title = title)
   return(p)
 }
